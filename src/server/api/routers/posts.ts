9597bd488500bb032b00cb85177655c46ddd25b8
Aaron Ford <aaron@aaronford.net>
2023-10-06 12:01:13 -0700

    Add Upstash ratelimiter to create post

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,3 +1,5 @@
+import { Ratelimit } from '@upstash/ratelimit'
+import { Redis } from '@upstash/redis'
 import type { User } from '@clerk/nextjs/server'
 import { clerkClient } from '@clerk/nextjs/server'
 import { TRPCError } from '@trpc/server'
@@ -7,6 +9,13 @@ import {
   privateProcedure,
   publicProcedure
 } from '~/server/api/trpc'
+
+// Create a new ratelimiter, that allows 3 requests per 1 minute
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(3, '1 m'),
+  analytics: true
+})
 
 const filterUserForClient = (user: User) => {
   return {
@@ -59,6 +68,12 @@ export const postsRouter = createTRPCRouter({
     )
     .mutation(async ({ ctx, input }) => {
       const authorId = ctx.userId
+      const { success } = await ratelimit.limit(authorId)
+
+      if (!success) {
+        throw new TRPCError({ code: 'TOO_MANY_REQUESTS' })
+      }
+
       const post = await ctx.db.post.create({
         data: {
           content: input.content,
